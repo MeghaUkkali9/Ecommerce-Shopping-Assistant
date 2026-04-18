e42aeec7dd364c80b5ccb40c719c82ecd9e6fb4f
MeghaUkkali9 <meghaukkali99@gmail.com>
2026-04-18 13:38:58 +0530

    updating retriever

--- shopping_assistant/retriever/retrieval.py
+++ shopping_assistant/retriever/retrieval.py
@@ -10,18 +10,14 @@ class Retriever:
         self.vectore_store = None
         
     def __load_env_variables(self):
-        required_vars = ["OPENAI_API_KEY", "ASTRA_DB_API_ENDPOINT", "ASTRA_DB_APPLICATION_TOKEN", "ASTRA_DB_KEYSPACE"]
-        
-        missing_vars = [variable for variable in required_vars if os.getenv(variable) is None]
-        if missing_vars:
-            raise EnvironmentError(f"Missing environment variables: {missing_vars}")
-        
-        self.openai_api_key = self.model_loader.api_key_mgr.get_openai_api_key()
-        self.astra_db_api_endpoint = self.model_loader.api_key_mgr.get_astra_db_api_endpoint()
-        self.astra_db_application_token = self.model_loader.api_key_mgr.get_astra_db_application_token()
-        self.astra_db_keyspace = self.model_loader.api_key_mgr.get_astra_db_keyspace()    
+        api_key_mgr = self.model_loader.api_key_mgr
+
+        self.openai_api_key = api_key_mgr.get_openai_api_key()
+        self.astra_db_api_endpoint = api_key_mgr.get_astra_db_api_endpoint()
+        self.astra_db_application_token = api_key_mgr.get_astra_db_application_token()
+        self.astra_db_keyspace = api_key_mgr.get_astra_db_keyspace() 
     
-    def __load_retriever(self):
+    def load_retriever(self):
         if self.vectore_store is None:
             self.vectore_store = AstraDBVectorStore(
                 collection_name=self.model_loader.config["astra_db"]["collection_name"],
@@ -35,10 +31,11 @@ class Retriever:
             config = self.model_loader.config
             top_k = self.model_loader.config["retriever"]["top_k"] if "retriever" in config and "top_k" in config["retriever"] else 3
             self.retriever = self.vectore_store.as_retriever(search_kwargs={"k": top_k})
-            return self.retriever
             
-    def call_retriever(self, query):
-        retriever = self.__load_retriever()
+        return self.retriever
+            
+    def retrieve(self, query):
+        retriever = self.load_retriever()
         response = retriever.invoke(query)
         return response
         
